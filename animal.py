932a1288b35c91834b28da4e7e5b0d58284ce8f9
Crispinb56 <Crispinb@oregonstate.edu>
2024-09-30 14:13:33 -0700

    Hi

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Bryce M Crispin
+# GitHub username: Crispinb56
+# Date: 09/30/2024
+# Description: Asks the user for their favorite animal and then prints out that
+
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
